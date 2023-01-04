f126806cd5cef91c7ace529643723c253cec96cd
github-webtest <81388699+github-webtest@users.noreply.github.com>
2023-01-04 18:37:57 +0300

    index

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var skor = 0;
+var tokats = Math.floor(Math.random() * 3 + 1);
+
+function tokat() {
+	document.getElementById("serhat_img").style.animationName = "tokat";
+	skor++;
+	document.getElementById("serhat_img").onclick = hello;
+	if (tokats == 1){
+		document.getElementById("tokat1").play();
+		tokats = Math.floor(Math.random() * 3 + 1);
+	} else if (tokats == 2){
+		document.getElementById("tokat2").play();
+		tokats = Math.floor(Math.random() * 3 + 1);
+	} else if (tokats == 3){
+		document.getElementById("tokat3").play();
+		tokats = Math.floor(Math.random() * 3 + 1);
+	}
+	setTimeout(function(){
+		document.getElementById("serhat_img").style.animationName = "";
+		document.getElementById("serhat_img").onclick = tokat;
+	}, 500);
+}
+
+function hello(){
+	
+}
+
+var interval = setInterval(function(){
+	document.getElementById("skor").innerText = skor;
+	document.getElementById("en_skor").innerText = localStorage.getItem("en_skor");
+	if (skor > localStorage.getItem("en_skor")) {
+		localStorage.setItem("en_skor", skor);
+	} else if (localStorage.getItem("en_skor") == null) {
+		localStorage.setItem("en_skor", skor);
+	}
+}, 1);
